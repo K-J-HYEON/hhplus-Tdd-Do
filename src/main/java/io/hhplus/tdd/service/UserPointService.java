ef565b6d01010e453071413cc4373c59d4fe2b22
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-03-20 00:49:27 +0900

    UserPointService 메소드명 변경

--- src/main/java/io/hhplus/tdd/service/UserPointService.java
+++ src/main/java/io/hhplus/tdd/service/UserPointService.java
@@ -5,9 +5,9 @@ import io.hhplus.tdd.dto.response.UserPointRespDto;
 import java.util.List;
 
 public interface UserPointService {
-    List<UserPointRespDto> getUserPoint(Long id);
+    List<UserPointRespDto> point(Long id);
 
-    List<UserPointRespDto> increaseUserPoint(Long id, Long amount);
+    List<UserPointRespDto> charge(Long id, Long amount);
 
-    List<UserPointRespDto> decreaseUserPoint(Long id, Long amount);
+    List<UserPointRespDto> use(Long id, Long amount);
 }
