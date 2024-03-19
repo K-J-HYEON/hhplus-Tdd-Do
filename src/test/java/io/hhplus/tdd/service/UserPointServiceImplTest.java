01e8b16723de0ad49efbc2620b256b56574b6cf7
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-03-19 23:40:39 +0900

    UserPointServiceImplTest 수정

--- src/test/java/io/hhplus/tdd/service/UserPointServiceImplTest.java
+++ src/test/java/io/hhplus/tdd/service/UserPointServiceImplTest.java
@@ -14,10 +14,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
 import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
-//인자로 확장할 Extension을 명시. SpringExtension.class 또는 MockitoExtension.class를 많이 사용
 @ExtendWith(MockitoExtension.class)
 public class UserPointServiceImplTest {
 
@@ -41,7 +40,7 @@ public class UserPointServiceImplTest {
     @Test
     @DisplayName("사용자 포인트 조회")
     void getUserPointTest() {
-        // given
+
         Long userId = 1L;
         Long point = 1L;
         Long updateMills = 1L;
@@ -49,7 +48,6 @@ public class UserPointServiceImplTest {
         UserPoint expectResult = new UserPoint(userId, point, updateMills);
         List<UserPointRespDto> result = userPointService.getUserPoint(1L);
 
-        // then
         assertThat(result.equals(userId)).isEqualTo(expectResult.equals(userId));
         assertThat(result.equals(point)).isEqualTo(expectResult.equals(point));
     }
@@ -58,55 +56,84 @@ public class UserPointServiceImplTest {
     @DisplayName("사용자 포인트 충전/이용 내역 조회")
     void getPointHistoryTest() {
 
-        // given
         Long userId = 1L;
         Long amount = 1L;
         UserPointReqDto userPointReqDto = new UserPointReqDto(amount);
         userPointService.increaseUserPoint(userId, userPointReqDto.point());
 
-        // when
         List<PointHistory> userPointRespDto = pointHistoryService.getUserPointHistory(userId);
 
-        // then
         assertThat(userPointRespDto).isNotEmpty();
     }
 
-
-    // 수정
     @Test
-    @DisplayName("사용자 포인트 충전")
+    @DisplayName("사용자 포인트 충전성공")
     void increaseUserPoint() {
-
-        // given
-        Long userId = 1L;
-        Long amount = 1L;
-        UserPointReqDto userPointRequest = new UserPointReqDto(amount);
-
-        // when
-        List<UserPointRespDto> expectResult = userPointService.getUserPoint(1L);
-        List<UserPointRespDto> result = userPointService.increaseUserPoint(userId, amount);
-
-        // then
-        assertThat(expectResult).isEqualTo(1);
-        assertThat(result).isEqualTo(1);
+        UserPoint userPoint = new UserPoint(1L, 10L, System.currentTimeMillis());
+        assertDoesNotThrow(() -> {
+            userPointService.increaseUserPoint(userPoint.id(), userPoint.point());
+        });
     }
 
     @Test
-    @DisplayName("사용자 포인트 사용")
+    @DisplayName("사용자 포인트 충전실패 Case 1: ID가 null이거나 포인트가 null 인경우")
+    void increaseUserPointFailCaseOne() {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> {
+                    userPointService.increaseUserPoint(null, null);
+                });
+    }
+
+    @Test
+    @DisplayName("사용자 포인트 충전실패 Case 2: 충전 point가 0 이하일 경우")
+    void increaseUserPointFailCaseTwo() {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> {
+                    userPointService.increaseUserPoint(1L, -1000L);
+                });
+    }
+
+    @Test
+    @DisplayName("ID와 잔액이 존재할 때 사용자 포인트 사용 성공")
     void decreaseUserPointTest() {
+        UserPoint userPoint = new UserPoint(1L, 50L, System.currentTimeMillis());
+        assertDoesNotThrow(() -> {
+            userPointService.decreaseUserPoint(userPoint.id(), userPoint.point());
+        });
+    }
 
-        // givem
-        Long userId = 1L;
-        Long amount = 1L;
-        UserPointReqDto userPointReqDto = new UserPointReqDto(amount);
+    @Test
+    @DisplayName("포인트 사용 시 포인트 업데이트")
+    void decreaseUserPointUpdateTest() {
+        Long chargePoint = 500L;
+        UserPoint userPoint = new UserPoint(1L, chargePoint, System.currentTimeMillis());
+        List<UserPointRespDto> chargeUserPoint = userPointService.increaseUserPoint(userPoint.id(), userPoint.point());
 
-        // when
-        List<UserPointRespDto> result = userPointService.decreaseUserPoint(userId, amount);
-        List<UserPointRespDto> expectResult = userPointService.getUserPoint(userId);
+        Long usePoint = 100L;
+        List<UserPointRespDto> usedPoint = userPointService.decreaseUserPoint(userPoint.id(), userPoint.point());
+        assertThat(usedPoint).isEqualTo(chargePoint - usePoint);
+    }
 
+    @Test
+    @DisplayName("포인트 사용 실패 Case 1: ID 또는 사용 포인트가 null")
+    void decreaseUserPointFailCaseOne() {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> {
+                    userPointService.decreaseUserPoint(null, null);
+                }).withMessageContaining("ID가 null 이거나 point가 null입니다.");
+    }
 
-        // then
-        assertThat(result.equals(userId)).isEqualTo(expectResult.equals(userId));
-        assertThat(result.equals(amount)).isEqualTo(expectResult.equals(amount));
+    @Test
+    @DisplayName("포인트 사용 실패 Case 2: 사용 포인트 > 잔액 포인트")
+    void decreaseUserPointFailCaseTwo() {
+        UserPoint userPoint = new UserPoint(1L, 100L, System.currentTimeMillis());
+        List<UserPointRespDto> chargedUserPoint = userPointService.increaseUserPoint(userPoint.id(), userPoint.point());
+
+        Long useUserPoint = 1000L;
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> {
+                    // 수정해야함 - chargedUserPoint를 반영해야 함
+                    userPointService.decreaseUserPoint(userPoint.id(), useUserPoint);
+                }).withMessageContaining("포인트 잔액이 부족합니다.");
     }
 }
