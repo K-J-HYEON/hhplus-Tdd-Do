d192e9b020f27cb1e542045aba492bf5ba305bd7
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-03-20 19:58:57 +0900

    UserPointServiceTest 각 메서드 이름 변경

--- src/test/java/io/hhplus/tdd/service/UserPointServiceTest.java
+++ src/test/java/io/hhplus/tdd/service/UserPointServiceTest.java
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
@@ -38,8 +39,9 @@ public class UserPointServiceTest {
     }
 
     @Test
+    @Transactional
     @DisplayName("사용자 포인트 조회")
-    void getUserPointTest() {
+    void pointTest() {
 
         Long userId = 1L;
         Long point = 1L;
@@ -53,8 +55,9 @@ public class UserPointServiceTest {
     }
 
     @Test
+    @Transactional
     @DisplayName("사용자 포인트 충전/이용 내역 조회")
-    void getPointHistoryTest() {
+    void pointHistoryTest() {
 
         Long userId = 1L;
         Long amount = 1L;
@@ -67,8 +70,9 @@ public class UserPointServiceTest {
     }
 
     @Test
+    @Transactional
     @DisplayName("사용자 포인트 충전성공")
-    void increaseUserPoint() {
+    void chargeTest() {
         UserPoint userPoint = new UserPoint(1L, 10L, System.currentTimeMillis());
         assertDoesNotThrow(() -> {
             userPointService.charge(userPoint.id(), userPoint.point());
@@ -76,8 +80,9 @@ public class UserPointServiceTest {
     }
 
     @Test
+    @Transactional
     @DisplayName("사용자 포인트 충전실패 Case 1: ID가 null이거나 포인트가 null 인경우")
-    void increaseUserPointFailCaseOne() {
+    void chargeFailCaseOneTest() {
         assertThatExceptionOfType(IllegalArgumentException.class)
                 .isThrownBy(() -> {
                     userPointService.charge(null, null);
@@ -85,8 +90,9 @@ public class UserPointServiceTest {
     }
 
     @Test
+    @Transactional
     @DisplayName("사용자 포인트 충전실패 Case 2: 충전 point가 0 이하일 경우")
-    void increaseUserPointFailCaseTwo() {
+    void chargeFailCaseTwoTest() {
         assertThatExceptionOfType(IllegalArgumentException.class)
                 .isThrownBy(() -> {
                     userPointService.charge(1L, -1000L);
@@ -94,8 +100,9 @@ public class UserPointServiceTest {
     }
 
     @Test
+    @Transactional
     @DisplayName("ID와 잔액이 존재할 때 사용자 포인트 사용 성공")
-    void decreaseUserPointTest() {
+    void useTest() {
         UserPoint userPoint = new UserPoint(1L, 50L, System.currentTimeMillis());
         assertDoesNotThrow(() -> {
             userPointService.use(userPoint.id(), userPoint.point());
@@ -103,8 +110,9 @@ public class UserPointServiceTest {
     }
 
     @Test
+    @Transactional
     @DisplayName("포인트 사용 시 포인트 업데이트")
-    void decreaseUserPointUpdateTest() {
+    void usePointUpdateTest() {
         Long chargePoint = 500L;
         UserPoint userPoint = new UserPoint(1L, chargePoint, System.currentTimeMillis());
         List<UserPointRespDto> chargeUserPoint = userPointService.charge(userPoint.id(), userPoint.point());
@@ -115,8 +123,9 @@ public class UserPointServiceTest {
     }
 
     @Test
+    @Transactional
     @DisplayName("포인트 사용 실패 Case 1: ID 또는 사용 포인트가 null")
-    void decreaseUserPointFailCaseOne() {
+    void useFailCaseOneTest() {
         assertThatExceptionOfType(IllegalArgumentException.class)
                 .isThrownBy(() -> {
                     userPointService.use(null, null);
@@ -124,8 +133,9 @@ public class UserPointServiceTest {
     }
 
     @Test
+    @Transactional
     @DisplayName("포인트 사용 실패 Case 2: 사용 포인트 > 잔액 포인트")
-    void decreaseUserPointFailCaseTwo() {
+    void useFailCaseTwoTest() {
         UserPoint userPoint = new UserPoint(1L, 100L, System.currentTimeMillis());
         List<UserPointRespDto> chargedUserPoint = userPointService.charge(userPoint.id(), userPoint.point());
 
@@ -135,5 +145,6 @@ public class UserPointServiceTest {
                     // 수정해야함 - chargedUserPoint를 반영해야 함
                     userPointService.use(userPoint.id(), useUserPoint);
                 }).withMessageContaining("포인트 잔액이 부족합니다.");
+
     }
 }
